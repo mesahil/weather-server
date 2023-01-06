270b1d24a10089bcdbfa60f638fcfabcbc0cd7ce
mesahil <49798494+mesahil@users.noreply.github.com>
2023-01-06 12:50:03 +0530

    Update index.js
    cors added

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const connectDB = require("./DB/connect");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const city_route = require("./routes/city");
-
+app.use(cors());
 // app.get("/", (req, res) => {
 //   res.status(200).json({ test: "ok" });
 // });
